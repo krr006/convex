b80b0e7f0b738a99507829501586eb1c590ed537
krr006 <desper1609@gmail.com>
2023-09-18 20:20:50 +0300

    fix vector.py

--- vector.py
+++ vector.py
@@ -3,11 +3,5 @@ class Vector:
         self.x = end.x - beg.x
         self.y = end.y - beg.y
 
-    def __add__(self, other):
-        return Vector(self.x + other.x, self.y + other.y)
-
-    def __sub__():
-        return Vector(self.x - other.x, self.y - other.y)
-
     def dot(self, other):
         return self.x*other.y - other.x*self.y
